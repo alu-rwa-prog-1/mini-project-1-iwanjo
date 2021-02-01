1ba8183310eeecc543eee257093052a0c7beb2b7
iwanjo <kamanuian@gmail.com>
2021-02-01 14:31:39 +0200

    Centered appbar title, center widget

--- ALU_RWA_MiniProject1_Wk3/lib/Home.dart
+++ ALU_RWA_MiniProject1_Wk3/lib/Home.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("DoughnutNation"),
+        title: Center(
+            child: Text(
+          "DoughnutNation",
+          style: GoogleFonts.cabin(
+            textStyle:
+                TextStyle(color: Colors.white, letterSpacing: .5, fontSize: 24),
+          ),
+        )),
       ),
     );
   }
