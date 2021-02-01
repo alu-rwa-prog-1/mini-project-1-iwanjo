04356264ae9eb238a2d9a11a95c6551dea3d3a05
iwanjo <kamanuian@gmail.com>
2021-02-01 14:19:10 +0200

    Google fonts package via pubdev

--- ALU_RWA_MiniProject1_Wk3/lib/Home.dart
+++ ALU_RWA_MiniProject1_Wk3/lib/Home.dart
@@ -5,7 +5,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Doughnut Nation"),
+        title: Text("DoughnutNation"),
       ),
     );
   }
