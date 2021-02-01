1a47ab799dd1433e5b2ebde4a8cc16c95a5185da
iwanjo <kamanuian@gmail.com>
2021-02-01 14:51:28 +0200

    Added bottom navbar and side drawer

--- ALU_RWA_MiniProject1_Wk3/lib/Home.dart
+++ ALU_RWA_MiniProject1_Wk3/lib/Home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
+  Home({this.uid});
+  final String uid;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +17,65 @@ class Home extends StatelessWidget {
                 TextStyle(color: Colors.white, letterSpacing: .5, fontSize: 24),
           ),
         )),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.shopping_cart),
+            label: 'Shopping',
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      drawer: NavigateDrawer(
+        uid: this.uid,
+      ),
+    );
+  }
+}
+
+class NavigateDrawer extends StatefulWidget {
+  final String uid;
+  NavigateDrawer({Key key, this.uid}) : super(key: key);
+  @override
+  _NavigateDrawerState createState() => _NavigateDrawerState();
+}
+
+class _NavigateDrawerState extends State<NavigateDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Melo Cruz"),
+            accountEmail: Text("mcruz@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage('assets/melo.jpeg'),
+                            fit: BoxFit.fill)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
